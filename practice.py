e0dfa3e4adcc1a8f7a0015165e6c2c9bd73820fa
whides37 <w.hides37@gmail.com>
2026-02-25 11:10:53 +0900

    年齢を受け付けて回答を表示

--- practice.py
+++ practice.py
@@ -1,8 +1,16 @@
 import math
 
-#入力受付した文字を数字に変換するのを忘れない。
-num = float(input("半径を計算したい数字を入力してね: "))
+num = float(input("半径を計算したい数字を入力してね:"))
 
-menseki = int(num * num * math.pi)
-print("その円の面積は、だいたい", menseki)
+menseki = num * num * math.pi
+print("その円の面積は、だいたい", round(menseki, 2))
+print(f"その円の面積は、だいたい {menseki:.2f}")
 
+age = int(input("年齢を入力してね:"))
+
+if age <20:
+    print("あなたは未成年者ですね")
+elif 20 <= age < 100:
+    print("あなたは成年者ですね")
+elif age > 100:
+    print("あなたは大長寿ですね")
